5f628ca281c92bf41a4cbd8699d094ca65064c66
Jeff Prestes <jeffprestes@gmail.com>
2021-07-14 10:22:17 -0300

    add go modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jeffprestes/goethereumhelper
+
+go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.5
--- mockblockchain.go
+++ mockblockchain.go
@@ -1,6 +1,7 @@
 package goethereumhelper
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"math/big"
 
@@ -14,6 +15,7 @@ import (
 func GetMockBlockchain() (auth *bind.TransactOpts, backend *backends.SimulatedBackend, coinbaseAccountPrivateKey *ecdsa.PrivateKey) {
 	coinbaseAccountPrivateKey, _ = crypto.GenerateKey()
 	auth = bind.NewKeyedTransactor(coinbaseAccountPrivateKey)
+	auth.Context = context.Background()
 	alloc := make(core.GenesisAlloc)
 	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(9000000000000000)}
 	backend = backends.NewSimulatedBackend(alloc, 90000000)
